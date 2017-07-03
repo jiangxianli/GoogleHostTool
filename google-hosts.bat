8e1cee9d5f97a1416ac6a18f6dd6fd1a1d14163d
jiangxianli <997204035@qq.com>
2017-07-03 16:55:29 +0800

    fix bug

--- google-hosts.bat
+++ google-hosts.bat
@@ -1,5 +1,5 @@
 powershell -noprofile Set-ExecutionPolicy Unrestricted
-set SHEEL_PATH=%cd%\google-hosts.ps1
+set SHEEL_PATH=%~dp0\google-hosts.ps1
 PowerShell -file %SHEEL_PATH%
 ipconfig/flushdns
 start  https://www.google.com.hk
